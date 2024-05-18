d7bbeac2b15e6e0e5629f2fd60c73b94fb116d99
morda7 <artem.moryakov7@mail.ru>
2024-05-18 22:02:57 +0300

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,4 @@
+numbers = [4, 5, 5, 6, 6, 7]
+a = sum(numbers) / len(numbers)
+print(a)
+print(type(str(a)))
---
+++ 31.py
@@ -0,0 +1,5 @@
+print(123)
+numbers = 123
+str(numbers)
+str = "49230743828"
+print(len(str))
