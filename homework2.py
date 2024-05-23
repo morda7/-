f11813d24c5b6bfe2bc884a46cc81a31c990e09b
morda7 <artem.moryakov7@mail.ru>
2024-05-23 21:26:40 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+name_course = 'Python'
+completed_homework = 12
+hours_spent = 1.5
+execution_time = hours_spent / completed_homework
+print('Курс:', name_course,",", 'всего задач:', completed_homework,",", 'затрачено часов:', hours_spent,",", 'среднее время выполнения:', execution_time, 'часа.')
