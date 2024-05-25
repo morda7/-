c89d82f16a679aab6498eb5ea9e180eef4e8f686
morda7 <artem.moryakov7@mail.ru>
2024-05-25 16:59:44 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,8 @@
+my_string = input('капибара любит траву ')
+my_string = 'капибара любит траву'
+print(len(my_string))
+print('капибара любит траву' .upper())
+print('капибара любит траву' .lower())
+print('капибара любит траву' .replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
