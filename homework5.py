7971fad41cd01469a877c529dcf194b0fe5d265e
morda7 <artem.moryakov7@mail.ru>
2024-05-26 11:20:39 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,7 @@
+immutable_var = (1, 2, 3, 4)
+print(type(immutable_var))
+print(immutable_var)
+#immutable_var[0] = 100
+mutable_list = ([1, 2, 4, 6,], 7, 8)
+mutable_list[0][0] = 3
+print(mutable_list)
