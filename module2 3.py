f80e0cbacc307767686d77ad836a623966730deb
morda7 <artem.moryakov7@mail.ru>
2024-06-08 11:52:53 +0300

    Add files via upload

---
+++ module2 3.py
@@ -0,0 +1,18 @@
+c = 0
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+while my_list[c] >= 0:
+    if my_list[c] == 0:
+        c += 1
+        continue
+        print(my_list[c])
+        c += 1
+
+
+
+
+
+
+
+
+
+
